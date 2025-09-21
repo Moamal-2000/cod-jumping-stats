f366e005a3fdd62eb047c5e5cfc3d9cfa59b7ff1
Moamal-2000 <moamalalaapro1@gmail.com>
2025-09-21 17:48:17 +0300

    refactor(OnlinePlayerItem): improve readability and add safe fallbacks for admin level and ping values

--- frontend/src/Components/Pages/ServersPage/AllServers/GameType/ServerCard/ServerCardPlayersSection/OnlinePlayersList/OnlinePlayerItem/OnlinePlayerItem.jsx
+++ frontend/src/Components/Pages/ServersPage/AllServers/GameType/ServerCard/ServerCardPlayersSection/OnlinePlayersList/OnlinePlayerItem/OnlinePlayerItem.jsx
@@ -1,31 +1,32 @@
+import SvgIcon from "@/Components/Shared/SvgIcon";
 import { getColoredName } from "@/Functions/components";
 import Link from "next/link";
 import s from "./OnlinePlayerItem.module.scss";
-import SvgIcon from "@/Components/Shared/SvgIcon";
 
 const OnlinePlayerItem = ({ player, server }) => {
   const isCod4 = server.game_type === "COD4";
+  const playerName = getColoredName(player.playername || "Unknown Player");
+
+  function handleClick(event) {
+    if (isCod4) event.preventDefault();
+  }
 
   return (
     <Link
       href={`/player/${player.playerid}`}
       className={`${s.playerItem} ${isCod4 ? s.cod4 : ""}`}
-      onClick={(event) => (isCod4 ? event.preventDefault() : "")}
+      onClick={handleClick}
     >
-      <div className={s.playerName}>
-        {getColoredName(player.playername || "Unknown Player")}
-      </div>
+      <strong className={s.playerName}>{playerName}</strong>
+
       <div className={s.playerInfo}>
-        {player.admin && (
-          <span className={s.playerAdminLevel}>
-            <SvgIcon name="shield" /> {player.admin}
-          </span>
-        )}
-        {player.ping && (
-          <span className={s.playerPing}>
-            <SvgIcon name="ping" /> {player.ping}ms
-          </span>
-        )}
+        <span className={s.playerAdminLevel}>
+          <SvgIcon name="shield" /> {player?.admin || "N/A"}
+        </span>
+
+        <span className={s.playerPing}>
+          <SvgIcon name="ping" /> {player?.ping || 0}ms
+        </span>
       </div>
     </Link>
   );
