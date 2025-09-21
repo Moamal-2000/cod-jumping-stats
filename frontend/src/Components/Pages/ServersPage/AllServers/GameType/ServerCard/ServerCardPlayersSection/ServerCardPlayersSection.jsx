a19c77552468675608b5604f20f74ad81c92205b
Moamal-2000 <moamalalaapro1@gmail.com>
2025-09-21 17:27:41 +0300

    init: ServerCardPlayersSection component

---
+++ frontend/src/Components/Pages/ServersPage/AllServers/GameType/ServerCard/ServerCardPlayersSection/ServerCardPlayersSection.jsx
@@ -0,0 +1,9 @@
+import s from './ServerCard.module.scss'
+
+const ServerCardPlayersSection = () => {
+  return (
+    <div>ServerCardPlayersSection</div>
+  )
+}
+
+export default ServerCardPlayersSection
