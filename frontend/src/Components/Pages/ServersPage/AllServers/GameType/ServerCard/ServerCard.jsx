4ba819126c4df95c20c9906d367a8edf91ee1bf2
Moamal-2000 <moamalalaapro1@gmail.com>
2025-09-21 17:31:49 +0300

    refactor(ServerCard): extract players section into ServerCardPlayersSection component

--- frontend/src/Components/Pages/ServersPage/AllServers/GameType/ServerCard/ServerCard.jsx
+++ frontend/src/Components/Pages/ServersPage/AllServers/GameType/ServerCard/ServerCard.jsx
@@ -1,57 +1,18 @@
 "use client";
 
-import SvgIcon from "@/Components/Shared/SvgIcon";
-import { getColoredName } from "@/Functions/components";
-import Link from "next/link";
 import s from "./ServerCard.module.scss";
 import ServerCardHeader from "./ServerCardHeader/ServerCardHeader";
 import ServerCardMapSection from "./ServerCardMapSection/ServerCardMapSection";
+import ServerCardPlayersSection from "./ServerCardPlayersSection/ServerCardPlayersSection";
 
 const ServerCard = ({ server }) => {
-  const hasPlayers =
-    server.online && server.players && server.players.length > 0;
   const isCod4 = server.game_type === "COD4";
 
   return (
     <div key={`${server.ip}-${server.port}`} className={s.serverCard}>
       <ServerCardHeader server={server} />
       <ServerCardMapSection server={server} isCod4={isCod4} />
-
-      {/* Players Section */}
-      <div className={s.playersSection}>
-        {hasPlayers && (
-          <div className={s.playersList}>
-            {server.players.map((player, index) => (
-              <Link
-                key={index}
-                href={`/player/${player.playerid}`}
-                className={`${s.playerItem} ${isCod4 ? s.cod4 : ""}`}
-                onClick={(event) => (isCod4 ? event.preventDefault() : "")}
-              >
-                <div className={s.playerName}>
-                  {getColoredName(player.playername || "Unknown Player")}
-                </div>
-                <div className={s.playerInfo}>
-                  {player.admin && (
-                    <span className={s.playerAdminLevel}>
-                      <SvgIcon name="shield" /> {player.admin}
-                    </span>
-                  )}
-                  {player.ping && (
-                    <span className={s.playerPing}>
-                      <SvgIcon name="ping" /> {player.ping}ms
-                    </span>
-                  )}
-                </div>
-              </Link>
-            ))}
-          </div>
-        )}
-
-        {!hasPlayers && (
-          <p className={s.noPlayersText}>This server is currently empty</p>
-        )}
-      </div>
+      <ServerCardPlayersSection server={server} isCod4={isCod4} />
     </div>
   );
 };
--- frontend/src/Components/Pages/ServersPage/AllServers/GameType/ServerCard/ServerCardPlayersSection/ServerCardPlayersSection.jsx
+++ frontend/src/Components/Pages/ServersPage/AllServers/GameType/ServerCard/ServerCardPlayersSection/ServerCardPlayersSection.jsx
@@ -1,9 +1,48 @@
-import s from './ServerCard.module.scss'
+import SvgIcon from "@/Components/Shared/SvgIcon";
+import { getColoredName } from "@/Functions/components";
+import Link from "next/link";
+import s from "./ServerCard.module.scss";
 
-const ServerCardPlayersSection = () => {
+const ServerCardPlayersSection = ({ server, isCod4 }) => {
+  const hasPlayers =
+    server.online && server.players && server.players.length > 0;
+
   return (
-    <div>ServerCardPlayersSection</div>
-  )
-}
+    <div className={s.playersSection}>
+      {hasPlayers && (
+        <div className={s.playersList}>
+          {server.players.map((player, index) => (
+            <Link
+              key={index}
+              href={`/player/${player.playerid}`}
+              className={`${s.playerItem} ${isCod4 ? s.cod4 : ""}`}
+              onClick={(event) => (isCod4 ? event.preventDefault() : "")}
+            >
+              <div className={s.playerName}>
+                {getColoredName(player.playername || "Unknown Player")}
+              </div>
+              <div className={s.playerInfo}>
+                {player.admin && (
+                  <span className={s.playerAdminLevel}>
+                    <SvgIcon name="shield" /> {player.admin}
+                  </span>
+                )}
+                {player.ping && (
+                  <span className={s.playerPing}>
+                    <SvgIcon name="ping" /> {player.ping}ms
+                  </span>
+                )}
+              </div>
+            </Link>
+          ))}
+        </div>
+      )}
 
-export default ServerCardPlayersSection
+      {!hasPlayers && (
+        <p className={s.noPlayersText}>This server is currently empty</p>
+      )}
+    </div>
+  );
+};
+
+export default ServerCardPlayersSection;
