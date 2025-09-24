063a67e70dd212fe2018d8215450c80d1e07b35a
Moamal-2000 <moamalalaapro1@gmail.com>
2025-09-24 04:36:21 +0300

    update(manualBadges): add some 'Winner', 'Bug Hunter', and 'Creator' badges

--- frontend/src/Data/manualBadges.js
+++ frontend/src/Data/manualBadges.js
@@ -1,7 +1,7 @@
-export const eventWinnerIds = [125382];
-export const bugHunterIds = [108468];
+export const eventWinnerIds = [46, 125382, 127166, 136177, 123104, 135795, 128523];
+export const bugHunterIds = [1, 108468];
 export const communityHelperIds = [1, 2, 46, 46077, 108468, 125382, 128567];
-export const contentCreatorIds = [46077];
+export const contentCreatorIds = [46, 50, 46077, 125382, 29950, 123104];
 export const mappersIds = [
   1, 2, 6, 10, 33, 46, 201, 625, 318, 545, 550, 2416, 6381, 13694, 2059, 10159,
   19128, 20314, 41299, 46077, 113380, 115092, 123104, 122405, 125382, 128523,
