3552edf3e39ad49b94aea5265fa4f0354d1eca15
Moamal-2000 <moamalalaapro1@gmail.com>
2025-09-27 00:19:19 +0300

    feat(OnlinePlayersList): sort players by admin level in descending order

--- frontend/src/Components/Pages/ServersPage/AllServers/GameType/ServerCard/ServerCardPlayersSection/OnlinePlayersList/OnlinePlayersList.jsx
+++ frontend/src/Components/Pages/ServersPage/AllServers/GameType/ServerCard/ServerCardPlayersSection/OnlinePlayersList/OnlinePlayersList.jsx
@@ -2,9 +2,11 @@ import OnlinePlayerItem from "./OnlinePlayerItem/OnlinePlayerItem";
 import s from "./OnlinePlayersList.module.scss";
 
 const OnlinePlayersList = ({ server }) => {
+  const sortBAdminPlayers = server.players.sort((a, b) => b.admin - a.admin);
+
   return (
     <div className={s.playersList}>
-      {server.players.map((player, index) => (
+      {sortBAdminPlayers.map((player, index) => (
         <OnlinePlayerItem key={index} player={player} server={server} />
       ))}
     </div>
