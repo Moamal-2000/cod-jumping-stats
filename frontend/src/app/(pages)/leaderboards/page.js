3add242749f82cb928b0f056d397d20249a31ade
Moamal-2000 <moamalalaapro1@gmail.com>
2025-09-27 00:49:46 +0300

    chore(LeaderboardsPage): remove unnecessary Suspense wrapper

--- frontend/src/app/(pages)/leaderboards/page.js
+++ frontend/src/app/(pages)/leaderboards/page.js
@@ -1,17 +1,12 @@
-"use client";
-
 import FiltersSection from "@/Components/Pages/Leaderboards/FiltersSection/FiltersSection";
 import LeaderBoard from "@/Components/Pages/Leaderboards/LeaderBoard/LeaderBoard";
-import { Suspense } from "react";
 
 const LeaderboardsPage = () => {
   return (
     <div className="container">
       <main>
-        <Suspense fallback={<div>Loading...</div>}>
-          <FiltersSection />
-          <LeaderBoard />
-        </Suspense>
+        <FiltersSection />
+        <LeaderBoard />
       </main>
     </div>
   );
