fc478bde5e8aec931cff0b455f6f7dfd1e170ad3
Moamal-2000 <moamalalaapro1@gmail.com>
2025-09-27 01:26:34 +0300

    chore(LeaderboardsPage): return Suspense wrapper

--- frontend/src/app/(pages)/leaderboards/page.js
+++ frontend/src/app/(pages)/leaderboards/page.js
@@ -1,12 +1,17 @@
+"use client";
+
 import FiltersSection from "@/Components/Pages/Leaderboards/FiltersSection/FiltersSection";
 import LeaderBoard from "@/Components/Pages/Leaderboards/LeaderBoard/LeaderBoard";
+import { Suspense } from "react";
 
 const LeaderboardsPage = () => {
   return (
     <div className="container">
       <main>
-        <FiltersSection />
-        <LeaderBoard />
+        <Suspense fallback={<div>Loading...</div>}>
+          <FiltersSection />
+          <LeaderBoard />
+        </Suspense>
       </main>
     </div>
   );
