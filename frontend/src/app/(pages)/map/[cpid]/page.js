3020eb73b565c22809ae1e67734ef8123c9e12d0
Moamal-2000 <moamalalaapro1@gmail.com>
2025-09-30 09:58:03 +0300

    feat(MapPage): include dynamic map name in page title as 'map name | JumpersHeaven'

--- frontend/src/app/(pages)/map/[cpid]/page.js
+++ frontend/src/app/(pages)/map/[cpid]/page.js
@@ -1,4 +1,18 @@
+import { jhApis } from "@/Api/jumpersHeaven";
 import MapDetailPage from "@/Components/Pages/MapDetail/MapDetailPage";
+import { decodeAsyncData, fetchMsgPackResponse } from "@/Functions/utils";
+
+export async function generateMetadata({ params }) {
+  const { cpid } = await params;
+
+  const response = await fetchMsgPackResponse({ url: jhApis().map.getAllMaps });
+  const data = await decodeAsyncData(response);
+  const map = data.find((map) => map.CpID === +cpid);
+
+  return {
+    title: `${map.Name} | JumpersHeaven`,
+  };
+}
 
 async function MapPage({ params }) {
   const { cpid } = await params;
