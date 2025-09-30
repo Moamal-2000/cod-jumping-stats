917cbb36a36f7cf7861ff02ee812df0abe105ace
Moamal-2000 <moamalalaapro1@gmail.com>
2025-09-30 10:53:19 +0300

    feat(PlayerPage): include dynamic player name in page title as 'player name | JumpersHeaven'

--- frontend/src/app/(pages)/player/[playerId]/page.js
+++ frontend/src/app/(pages)/player/[playerId]/page.js
@@ -1,4 +1,10 @@
 import PlayerProfile from "@/Components/Pages/PlayerProfile/PlayerProfile";
+import { generatePlayerMetadata } from "@/Functions/utils";
+
+export async function generateMetadata({ params }) {
+  const { playerId } = await params;
+  return generatePlayerMetadata({ playerId });
+}
 
 const PlayerPage = async ({ params }) => {
   const { playerId } = await params;
