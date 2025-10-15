1ee82ba8be6eae90f7e2f91029809465b5fcb158
Moamal-2000 <moamalalaapro1@gmail.com>
2025-10-15 03:44:39 +0300

    Revert "feat(servers page): switch page from SSG to SSR (without loading/error UIs)"
    This reverts commit e431b30a61d12ff19add1da7b1e8e7eca6e266af.

--- frontend/src/Components/Pages/ServersPage/AllServers/AllServers.jsx
+++ frontend/src/Components/Pages/ServersPage/AllServers/AllServers.jsx
@@ -1,9 +1,14 @@
 import { getCodServers, getGameTypes } from "@/Functions/utils";
 import GameType from "./GameType/GameType";
+import ServersLoadingError from "./ServersLoadingError/ServersLoadingError";
 
-const AllServers = ({ servers }) => {
+const AllServers = ({ servers, loading, error }) => {
   const groupedServers = getCodServers(servers);
   const gameTypes = getGameTypes(groupedServers);
+
+  if (loading || error) {
+    return <ServersLoadingError loading={loading} error={error} />;
+  }
 
   return gameTypes.map((gameType) => (
     <GameType
---
+++ frontend/src/Components/Pages/ServersPage/ServersPage.jsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { jhApis } from "@/Api/jumpersHeaven";
+import { decodeAsyncData, fetchMsgPackResponse } from "@/Functions/utils";
+import { useEffect, useState } from "react";
+import AllServers from "./AllServers/AllServers";
+
+const ServersPage = () => {
+  const [servers, setServers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchServers = async () => {
+      try {
+        const response = await fetchMsgPackResponse({
+          url: jhApis().player.getOnlinePlayers,
+          cache: "no-cache",
+        });
+
+        if (!response.ok) throw new Error("Failed to fetch server data");
+
+        const data = await decodeAsyncData(response);
+        setServers(data.Servers || []);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchServers();
+  }, []);
+
+  return <AllServers servers={servers} loading={loading} error={error} />;
+};
+
+export default ServersPage;
