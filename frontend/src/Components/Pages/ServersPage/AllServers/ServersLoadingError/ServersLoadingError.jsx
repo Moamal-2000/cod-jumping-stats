558c87264a82909f350f117a5079be5bdc9b38fb
Moamal-2000 <moamalalaapro1@gmail.com>
2025-10-15 03:46:13 +0300

    revert(ServersLoadingError): restore loading/error UI rendering based on props

--- frontend/src/Components/Pages/ServersPage/AllServers/ServersLoadingError/ServersLoadingError.jsx
+++ frontend/src/Components/Pages/ServersPage/AllServers/ServersLoadingError/ServersLoadingError.jsx
@@ -1,28 +1,32 @@
 import SkeletonCard from "../SkeletonCard/SkeletonCard";
 import s from "./ServersLoadingError.module.scss";
 
-const ServersLoadingError = () => {
-  return (
-    <>
-      <div className={s.serversGrid}>
-        {[...Array(10)].map((_, i) => (
-          <SkeletonCard key={i} />
-        ))}
-      </div>
-      <div className={s.serversGrid}>
-        {[...Array(11)].map((_, i) => (
-          <SkeletonCard key={i} />
-        ))}
-      </div>
-    </>
-  );
+const ServersLoadingError = ({ loading, error }) => {
+  if (loading) {
+    return (
+      <>
+        <div className={s.serversGrid}>
+          {[...Array(10)].map((_, i) => (
+            <SkeletonCard key={i} />
+          ))}
+        </div>
+        <div className={s.serversGrid}>
+          {[...Array(11)].map((_, i) => (
+            <SkeletonCard key={i} />
+          ))}
+        </div>
+      </>
+    );
+  }
 
-  // return (
-  //     <div className={s.errorContainer}>
-  //       <h3>Server Status Unavailable</h3>
-  //       <p>{error}</p>
-  //     </div>
-  //   );
+  if (error) {
+    return (
+      <div className={s.errorContainer}>
+        <h3>Server Status Unavailable</h3>
+        <p>{error}</p>
+      </div>
+    );
+  }
 };
 
 export default ServersLoadingError;
