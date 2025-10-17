413d15d6f6e3cdd6e1a79b8a68b704b6509c17b4
Moamal-2000 <moamalalaapro1@gmail.com>
2025-10-18 00:43:05 +0300

    feat(mapsThunk): use cache-first approach to improve loading performance for maps page

--- frontend/src/Redux/thunks/mapsThunk.js
+++ frontend/src/Redux/thunks/mapsThunk.js
@@ -1,10 +1,23 @@
 import { jhApis } from "@/Api/jumpersHeaven";
+import { MAPS_CACHE_EXPIRATION_TIME } from "@/Data/constants";
 import { cacheMapsLocally, decodeAsyncData } from "@/Functions/utils";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
 export const fetchMaps = createAsyncThunk(
   "globalSlice/fetchMaps",
   async (paramsObject) => {
+    let mapsLocal = localStorage.getItem("mapsData");
+
+    if (mapsLocal) {
+      const cachedData = JSON.parse(mapsLocal);
+      const isCacheExpire =
+        Date.now() - cachedData.timeStamp > MAPS_CACHE_EXPIRATION_TIME;
+
+      if (!isCacheExpire) {
+        return { mapsData: cachedData.maps, paramsObject };
+      }
+    }
+
     try {
       const response = await fetch(jhApis().map.getAllMaps, {
         headers: { Accept: "application/msgpack", "Accept-Encoding": "gzip" },
