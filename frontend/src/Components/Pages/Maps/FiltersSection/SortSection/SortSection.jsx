99b789fe78ff4b827ba306ed2e8d7a7caa001156
Moamal-2000 <moamalalaapro1@gmail.com>
2025-10-19 01:09:31 +0300

    revert(SortSection): restore original sort section layout and styles

--- frontend/src/Components/Pages/Maps/FiltersSection/SortSection/SortSection.jsx
+++ frontend/src/Components/Pages/Maps/FiltersSection/SortSection/SortSection.jsx
@@ -1,16 +1,8 @@
-import { setSortBy } from "@/Redux/slices/mapsSlice";
-import { useDispatch, useSelector } from "react-redux";
+import CustomSelectMenu from "@/Components/Shared/SelectMenus/CustomSelectMenu/CustomSelectMenu";
 import s from "./SortSection.module.scss";
 import SortView from "./SortView/SortView";
 
 const SortSection = ({ setPaginationNumber }) => {
-  const dispatch = useDispatch();
-  const { sortBy } = useSelector((state) => state.maps);
-
-  const handleSortChange = (e) => {
-    dispatch(setSortBy(e.target.value));
-  };
-
   return (
     <section className={s.sortSection}>
       <div className={s.sortWrapper}>
@@ -29,22 +21,7 @@ const SortSection = ({ setPaginationNumber }) => {
           </div>
         </label>
 
-        <select
-          id="sort-by"
-          value={sortBy}
-          onChange={handleSortChange}
-          className={s.sortSelect}
-        >
-          <option value="newest">Newest First</option>
-          <option value="oldest">Oldest First</option>
-          <option value="name-a-z">Name (A-Z)</option>
-          <option value="name-z-a">Name (Z-A)</option>
-          <option value="43-difficulty">43 fps (Difficulty)</option>
-          <option value="76-difficulty">76 fps (Difficulty)</option>
-          <option value="125-difficulty">125 fps (Difficulty)</option>
-          <option value="250-difficulty">250 fps (Difficulty)</option>
-          <option value="333-difficulty">333 fps (Difficulty)</option>
-        </select>
+        <CustomSelectMenu />
       </div>
 
       <SortView setPaginationNumber={setPaginationNumber} />
