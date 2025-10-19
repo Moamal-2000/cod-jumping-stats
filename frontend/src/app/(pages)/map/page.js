df8b4b085b032e1463db63a44cc02ea60f86e393
Moamal-2000 <moamalalaapro1@gmail.com>
2025-10-20 02:42:27 +0300

    refactor(MapDetailPage): wrap component with React Suspense to show loading fallback

--- frontend/src/app/(pages)/map/page.js
+++ frontend/src/app/(pages)/map/page.js
@@ -1,7 +1,12 @@
 import MapDetailPage from "@/Components/Pages/MapDetail/MapDetailPage";
+import { Suspense } from "react";
 
 async function MapPage() {
-  return <MapDetailPage />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <MapDetailPage />
+    </Suspense>
+  );
 }
 
 export default MapPage;
