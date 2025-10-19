ee8133dad45736c160605f7b2ec0bf2011f4805f
Moamal-2000 <moamalalaapro1@gmail.com>
2025-10-19 03:18:44 +0300

    style(SortSection): make component responsive across all screen sizes

--- frontend/src/Components/Pages/Maps/FiltersSection/SortSection/SortSection.jsx
+++ frontend/src/Components/Pages/Maps/FiltersSection/SortSection/SortSection.jsx
@@ -6,23 +6,26 @@ import SortView from "./SortView/SortView";
 const SortSection = ({ setPaginationNumber }) => {
   return (
     <section className={s.sortSection}>
-      <div className={s.sortWrapper}>
-        <label htmlFor="sort-by" className={s.label}>
-          Sort Maps By:
-          <div className={s.tooltip}>
-            <div className={s.icon}>
-              <svg>
-                <use href="/icons-sprite.svg#questionMark" />
-              </svg>
+      <div className={s.leftSide}>
+        <div className={s.sortWrapper}>
+          <label htmlFor="sort-by" className={s.label}>
+            Sort Maps By:
+            <div className={s.tooltip}>
+              <div className={s.icon}>
+                <svg>
+                  <use href="/icons-sprite.svg#questionMark" />
+                </svg>
+              </div>
+
+              <p className={s.tooltipText}>
+                Choose how to order the displayed maps
+              </p>
             </div>
+          </label>
 
-            <p className={s.tooltipText}>
-              Choose how to order the displayed maps
-            </p>
-          </div>
-        </label>
+          <CustomSelectMenu />
+        </div>
 
-        <CustomSelectMenu />
         <MapsSearchInput />
       </div>
 
