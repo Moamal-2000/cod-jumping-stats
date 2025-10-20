ac66b7132021cde2cc7bd9be74c36226e8b87e1f
Moamal-2000 <moamalalaapro1@gmail.com>
2025-10-20 03:01:42 +0300

    perf(MapCard, MapCard2): add unique key for each FPS difficulty to improve rendering performance

--- frontend/src/Components/Pages/Maps/MapCard2/MapCard2.jsx
+++ frontend/src/Components/Pages/Maps/MapCard2/MapCard2.jsx
@@ -49,7 +49,7 @@ const MapCard2 = ({ mapData, mapsScroll, mapsData, lastMapRef, index }) => {
           <div className={s.fpsDifficulties}>
             {JUMP_FPS.map((fps) => {
               return (
-                <div className={s.fpsDifficulty}>
+                <div key={fps} className={s.fpsDifficulty}>
                   <span className={s.fps}>{fps}</span>
                   <span className={s.difficulty}>
                     {Difficulty?.[fps]?.Difficulty < 0
