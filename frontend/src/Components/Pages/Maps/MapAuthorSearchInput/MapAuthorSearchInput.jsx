8d53d86875fa7e783e5af7e1c99f22abf03dafdc
Moamal-2000 <moamalalaapro1@gmail.com>
2025-10-22 07:35:41 +0300

    init: MapAuthorSearchInput component

---
+++ frontend/src/Components/Pages/Maps/MapAuthorSearchInput/MapAuthorSearchInput.jsx
@@ -0,0 +1,9 @@
+import s from './MapAuthorSearchInput.module.scss'
+
+const MapAuthorSearchInput = () => {
+  return (
+    <div>MapAuthorSearchInput</div>
+  )
+}
+
+export default MapAuthorSearchInput
