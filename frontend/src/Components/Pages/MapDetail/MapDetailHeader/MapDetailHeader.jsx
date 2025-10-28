34bf6cc86c85325e52212bad67ed138b0129f12a
Moamal-2000 <moamalalaapro1@gmail.com>
2025-10-28 11:09:21 +0300

    feat(MapDetailHeader): add route tabs for maps that contain multiple routes

--- frontend/src/Components/Pages/MapDetail/MapDetailHeader/MapDetailHeader.jsx
+++ frontend/src/Components/Pages/MapDetail/MapDetailHeader/MapDetailHeader.jsx
@@ -1,9 +1,26 @@
 import MapImage from "@/Components/Shared/Images/MapImage/MapImage";
+import { fetchMaps } from "@/Redux/thunks/mapsThunk";
+import Link from "next/link";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import s from "./MapDetailHeader.module.scss";
 
-const MapDetailHeader = ({
-  mapData: { Name, Author, Released, Type, Ender },
-}) => {
+const MapDetailHeader = ({ mapData }) => {
+  const { Name, Author, Released, Type, Ender } = mapData;
+
+  const allMaps = useSelector((s) => s.maps.allMaps);
+  const dispatch = useDispatch();
+
+  const otherRoutes = allMaps.filter((map) => {
+    return map.Name === Name && map.Ender !== Ender;
+  });
+
+  useEffect(() => {
+    if (allMaps.length > 0) return;
+
+    dispatch(fetchMaps());
+  }, []);
+
   return (
     <div className={s.header}>
       <div className={s.mapInfo}>
@@ -14,7 +31,7 @@ const MapDetailHeader = ({
         <div className={s.mapDetails}>
           <h1 className={s.mapName}>
             {Name}
-            {Ender && <span className={s.ender}>({Ender})</span>}
+            {Ender && <span className={s.ender}>{Ender}</span>}
           </h1>
 
           <div className={s.mapMeta}>
@@ -38,6 +55,22 @@ const MapDetailHeader = ({
             </div>
           </div>
         </div>
+
+        {otherRoutes.length > 1 && (
+          <div className={s.otherRoutes}>
+            <div className={s.otherRoutesLabel} aria-hidden="true">
+              Routes from this map
+            </div>
+
+            <div className={s.otherRoutesList}>
+              {otherRoutes.map((route) => (
+                <Link key={route.CpID} href={`/map?mapid=${route.CpID}`}>
+                  {route.Ender}
+                </Link>
+              ))}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
