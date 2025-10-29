91ef4a458ff946834e43c5ed6212a6936fbf182b
Moamal-2000 <moamalalaapro1@gmail.com>
2025-10-29 05:23:35 +0300

    refactor(PlayerOverview): extract recent activity section into RecentActivity component

--- frontend/src/Components/Pages/PlayerProfile/PlayerOverview/PlayerOverview.jsx
+++ frontend/src/Components/Pages/PlayerProfile/PlayerOverview/PlayerOverview.jsx
@@ -1,69 +1,17 @@
 "use client";
 
-import Link from "next/link";
-import { useSelector } from "react-redux";
 import PlayerInfo from "./PlayerInfo/PlayerInfo";
-import s from "./PlayerOverview.module.scss";
+import RecentActivity from "./RecentActivity/RecentActivity";
 import TopRunsPerFps from "./TopRunsPerFps/TopRunsPerFps";
 
 const PlayerOverview = () => {
-  const performanceStats = useSelector((s) => s.playerProfile.performanceStats);
-  const topRunsCount = performanceStats?.RecentTops?.length;
-
   return (
-    <div className={s.tabContent}>
-      <div className={s.overviewTab}>
-        <PlayerInfo />
-        <TopRunsPerFps />
-
-        {/* Recent Activity */}
-        {performanceStats?.RecentTops && topRunsCount > 0 && (
-          <div className={s.recentActivitySection}>
-            <h2>Recent Activity</h2>
-            <p className={s.sectionDescription}>
-              Latest top {topRunsCount} finishes and achievements
-            </p>
-
-            <div className={s.recentActivityList}>
-              {performanceStats.RecentTops.map((run, index) => (
-                <Link
-                  href={`/map?mapid=${run.Cpid}`}
-                  className={s.recentActivityItem}
-                  key={index}
-                >
-                  <div className={s.activityIcon}>
-                    <svg aria-hidden="true">
-                      <use href="/icons-sprite.svg#star" />
-                    </svg>
-                  </div>
-                  <div className={s.activityContent}>
-                    <div className={s.activityTitle}>{run.MapName}</div>
-                    <div className={s.activityDetails}>
-                      <span className={s.activityRank}>Rank #{run.Rank}</span>
-                      <span className={s.activityFps}>{run.FPS} FPS</span>
-                      <span className={s.activityDate}>
-                        {formatDate(run.FinishDate)}
-                      </span>
-                    </div>
-                  </div>
-                </Link>
-              ))}
-            </div>
-          </div>
-        )}
-      </div>
-    </div>
+    <>
+      <PlayerInfo />
+      <TopRunsPerFps />
+      <RecentActivity />
+    </>
   );
 };
 
 export default PlayerOverview;
-
-function formatDate(dateString) {
-  if (!dateString) return "N/A";
-
-  try {
-    return new Date(dateString).toLocaleDateString();
-  } catch {
-    return dateString;
-  }
-}
--- frontend/src/Components/Pages/PlayerProfile/PlayerOverview/RecentActivity/RecentActivity.jsx
+++ frontend/src/Components/Pages/PlayerProfile/PlayerOverview/RecentActivity/RecentActivity.jsx
@@ -1,9 +1,60 @@
-import s from './RecentActivity.module.scss'
+"use client";
+
+import Link from "next/link";
+import { useSelector } from "react-redux";
+import s from "./RecentActivity.module.scss";
 
 const RecentActivity = () => {
-  return (
-    <div>RecentActivity</div>
-  )
-}
+  const performanceStats = useSelector((s) => s.playerProfile.performanceStats);
+  const topRunsCount = performanceStats?.RecentTops?.length;
 
-export default RecentActivity
+  return (
+    performanceStats?.RecentTops &&
+    topRunsCount > 0 && (
+      <div className={s.recentActivitySection}>
+        <h2>Recent Activity</h2>
+        <p className={s.sectionDescription}>
+          Latest top {topRunsCount} finishes and achievements
+        </p>
+
+        <div className={s.recentActivityList}>
+          {performanceStats.RecentTops.map((run, index) => (
+            <Link
+              href={`/map?mapid=${run.Cpid}`}
+              className={s.recentActivityItem}
+              key={index}
+            >
+              <div className={s.activityIcon}>
+                <svg aria-hidden="true">
+                  <use href="/icons-sprite.svg#star" />
+                </svg>
+              </div>
+              <div className={s.activityContent}>
+                <div className={s.activityTitle}>{run.MapName}</div>
+                <div className={s.activityDetails}>
+                  <span className={s.activityRank}>Rank #{run.Rank}</span>
+                  <span className={s.activityFps}>{run.FPS} FPS</span>
+                  <span className={s.activityDate}>
+                    {formatDate(run.FinishDate)}
+                  </span>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    )
+  );
+};
+
+export default RecentActivity;
+
+function formatDate(dateString) {
+  if (!dateString) return "N/A";
+
+  try {
+    return new Date(dateString).toLocaleDateString();
+  } catch {
+    return dateString;
+  }
+}
