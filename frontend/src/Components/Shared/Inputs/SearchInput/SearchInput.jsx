09b8bc3e57dc95e00557b426124519c2a36bec28
Moamal-2000 <moamalalaapro1@gmail.com>
2025-11-03 12:32:39 +0300

    refactor(SearchInput): clean up unused hooks, simplify component logic, and improve naming for readability

--- frontend/src/Components/Shared/Inputs/SearchInput/SearchInput.jsx
+++ frontend/src/Components/Shared/Inputs/SearchInput/SearchInput.jsx
@@ -6,50 +6,43 @@ import { useRef, useState } from "react";
 import s from "./SearchInput.module.scss";
 
 const SearchInput = ({ queryName, placeholder, label, id }) => {
-  const router = useRouter();
-  const pathname = usePathname();
   const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
 
   const [searchValue, setSearchValue] = useState(
     searchParams.get(queryName) || ""
   );
-  const [isSearching, setIsSearching] = useState(false);
-  const inputRef = useRef(null);
-  const searchTimeoutRef = useRef(null);
 
-  function handleSearchInput(event) {
+  const debounceRef = useRef(null);
+
+  function handleOnChange(event) {
     const inputValue = event.target.value;
     setSearchValue(inputValue);
 
-    clearTimeout(searchTimeoutRef?.current);
-    searchTimeoutRef.current = setTimeout(() => performSearch(inputValue), 300);
+    clearTimeout(debounceRef?.current);
+    debounceRef.current = setTimeout(() => performSearch(inputValue), 300);
   }
 
   function performSearch(searchValue) {
     if (!searchValue.trim()) {
-      setIsSearching(false);
       removeQueryString(queryName, searchParams, router, pathname);
       return;
     }
-    setIsSearching(true);
-
-    const searchLowerCased = searchValue.toLowerCase();
 
     createQueryString(
       queryName,
-      searchLowerCased,
+      searchValue.toLowerCase(),
       searchParams,
       router,
       pathname
     );
-    setIsSearching(false);
   }
 
   function handleClearSearch() {
-    if (inputRef.current) setSearchValue("");
+    setSearchValue("");
     removeQueryString(queryName, searchParams, router, pathname);
-    clearTimeout(searchTimeoutRef?.current);
-    setIsSearching(false);
+    clearTimeout(debounceRef?.current);
   }
 
   return (
@@ -64,17 +57,10 @@ const SearchInput = ({ queryName, placeholder, label, id }) => {
         className={s.searchInput}
         type="text"
         placeholder={placeholder}
-        onChange={handleSearchInput}
+        onChange={handleOnChange}
         value={searchValue}
-        ref={inputRef}
         id={id}
       />
-
-      {isSearching && (
-        <div className={s.searchLoadingIndicator}>
-          <div className={s.searchSpinner} />
-        </div>
-      )}
 
       <button
         onClick={handleClearSearch}
