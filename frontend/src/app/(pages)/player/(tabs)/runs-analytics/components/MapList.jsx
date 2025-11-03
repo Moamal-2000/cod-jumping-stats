dcfc8335c3c6beb2fcfdb04711eb78f56394951a
Moamal-2000 <moamalalaapro1@gmail.com>
2025-11-03 12:21:04 +0300

    feat(MapList): implement search functionality synced with URL query parameters

--- frontend/src/app/(pages)/player/(tabs)/runs-analytics/components/MapList.jsx
+++ frontend/src/app/(pages)/player/(tabs)/runs-analytics/components/MapList.jsx
@@ -1,14 +1,35 @@
 "use client";
+
 import SearchInput from "@/Components/Shared/Inputs/SearchInput/SearchInput";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 import styles from "../RunAnalytics.module.scss";
 
 const MapList = ({ allMaps, selectedCpid, onSelect }) => {
+  const [filteredMaps, setFilteredMaps] = useState(allMaps);
+
+  const searchParams = useSearchParams();
+  const mapName = searchParams.get("mapname") || "";
+
+  useEffect(() => {
+    if (mapName === "") {
+      setFilteredMaps(allMaps);
+      return;
+    }
+
+    const filteredMaps = allMaps.filter((map) =>
+      map.Name.toLowerCase().includes(mapName.toLowerCase())
+    );
+
+    setFilteredMaps(filteredMaps);
+  }, [mapName]);
+
   return (
     <div className={styles.leftPanel}>
       <SearchInput placeholder="Search map by name" queryName="mapname" />
 
       <div className={styles.mapsList} role="list">
-        {allMaps.map((map) => {
+        {filteredMaps.map((map) => {
           const active = selectedCpid === map.CpID;
 
           return (
