e77773bbbcae1463f7843c022babbb77940fb6b7
Moamal-2000 <moamalalaapro1@gmail.com>
2025-11-05 13:12:49 +0300

    fix(RunAnalytics): sync selected FPS with URL parameter

--- frontend/src/Components/Pages/PlayerProfile/RunAnalytics/RunAnalytics.jsx
+++ frontend/src/Components/Pages/PlayerProfile/RunAnalytics/RunAnalytics.jsx
@@ -80,6 +80,7 @@ const RunAnalytics = () => {
                 label="FPS"
                 onChange={handleFpsChange}
                 optionsData={fpsOptions}
+                value={selectedFps}
               />
             </div>
           </div>
