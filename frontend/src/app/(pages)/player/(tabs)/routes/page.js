8bb5442d8906f7a922716cae57545d819b8de15f
Moamal-2000 <moamalalaapro1@gmail.com>
2025-11-05 20:13:02 +0300

    refactor(RoutesTab): remove unnecessary <div> wrapper and delete unused SCSS files

--- frontend/src/app/(pages)/player/(tabs)/routes/page.js
+++ frontend/src/app/(pages)/player/(tabs)/routes/page.js
@@ -1,12 +1,7 @@
 import PlayerRouteCompletion from "@/Components/Pages/PlayerProfile/PlayerRouteCompletion/PlayerRouteCompletion";
-import s from "@/Components/Pages/PlayerProfile/RoutesTab.module.scss";
 
 const RoutesTab = () => {
-  return (
-    <div className={s.routeCompletionTab}>
-      <PlayerRouteCompletion />
-    </div>
-  );
+  return <PlayerRouteCompletion />;
 };
 
 export default RoutesTab;
