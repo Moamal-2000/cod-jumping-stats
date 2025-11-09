5b2b80335ba5d329d83c23c91e332f531a0243a8
Moamal-2000 <moamalalaapro1@gmail.com>
2025-11-10 00:08:01 +0300

    update(next.config): configure remotePatterns for external images

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,6 +4,22 @@ const nextConfig = {
   devIndicators: false,
   reactCompiler: true,
   productionBrowserSourceMaps: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.youtube.com",
+      },
+      {
+        protocol: "https",
+        hostname: "yt3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "yt3.ggpht.com",
+      },
+    ],
+  },
   async rewrites() {
     return [
       {
