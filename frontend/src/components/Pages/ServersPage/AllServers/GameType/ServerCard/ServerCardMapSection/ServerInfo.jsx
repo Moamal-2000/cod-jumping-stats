4a8c626e3cb8ebc1f7749967da1272a1087adc94
Moamal-2000 <moamalalaapro1@gmail.com>
2025-11-16 04:14:16 +0300

    feat(ServerInfo): display CoD2/CoD4 game type labels on server cards to make server info easier to understand

--- frontend/src/components/Pages/ServersPage/AllServers/GameType/ServerCard/ServerCardMapSection/ServerInfo.jsx
+++ frontend/src/components/Pages/ServersPage/AllServers/GameType/ServerCard/ServerCardMapSection/ServerInfo.jsx
@@ -5,22 +5,37 @@ const ServerInfo = ({ server }) => {
   const isCod4 = server.GameType === "COD4";
 
   return (
-    <div className={s.mapInfo}>
-      <span className={s.mapLabel}>
-        <svg aria-hidden="true">
-          <use href="/icons-sprite.svg#globe" />
-        </svg>{" "}
-        Map
-      </span>
+    <section className={s.serverInfo}>
+      <div className={s.infoItem}>
+        <span className={s.label}>
+          <svg aria-hidden="true">
+            <use href="/icons-sprite.svg#joystick" />
+          </svg>{" "}
+          Game
+        </span>
 
-      {isCod4 && <span className={s.mapName}>{server.Map}</span>}
+        <span className={`${s.gameName} ${isCod4 ? s.cod4 : s.cod2}`}>
+          {`Call of Duty ${isCod4 ? "4" : "2"}`}
+        </span>
+      </div>
 
-      {!isCod4 && (
-        <Link href={`/map?mapid=${server.MapID}`} className={s.mapName}>
-          {server.Map}
-        </Link>
-      )}
-    </div>
+      <div className={s.infoItem}>
+        <span className={s.label}>
+          <svg aria-hidden="true">
+            <use href="/icons-sprite.svg#globe" />
+          </svg>{" "}
+          Map
+        </span>
+
+        {isCod4 && <span className={s.mapName}>{server.Map}</span>}
+
+        {!isCod4 && (
+          <Link href={`/map?mapid=${server.MapID}`} className={s.mapName}>
+            {server.Map}
+          </Link>
+        )}
+      </div>
+    </section>
   );
 };
 
