193f03fa67ece1aad6d9ced61b441d1d51fb4929
Moamal-2000 <moamalalaapro1@gmail.com>
2025-11-28 16:43:59 +0300

    feat(PlayerLayout): add styled hero section with SEO-friendly content explaining page purpose

--- frontend/src/app/(pages)/player/layout.js
+++ frontend/src/app/(pages)/player/layout.js
@@ -1,12 +1,40 @@
+"use client";
+
+import s from "@/components/Pages/PlayerProfile/PlayerLayout.module.scss";
 import PlayerProfileLayout from "@/components/Pages/PlayerProfile/PlayerProfileLayout/PlayerProfileLayout";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 import { Suspense } from "react";
 
-const PlayerLayout = ({ children }) => {
+function PlayerLayout({ children }) {
+  const searchParams = useSearchParams();
+  const playerId = searchParams.get("playerid");
+  const isPlayerIdExist = playerId !== null;
+
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <PlayerProfileLayout>{children}</PlayerProfileLayout>
-    </Suspense>
+    <>
+      {!isPlayerIdExist && (
+        <main>
+          <div className="container">
+            <section className={s.heroSection}>
+              <h1>Players Overview</h1>
+              <p>
+                Explore all Jumpers Heaven <Link href="/players">players</Link>.
+                Click on a player to view detailed statistics, performance
+                history, and records.
+              </p>
+            </section>
+          </div>
+        </main>
+      )}
+
+      {isPlayerIdExist && (
+        <Suspense fallback={<div>Loading...</div>}>
+          <PlayerProfileLayout>{children}</PlayerProfileLayout>
+        </Suspense>
+      )}
+    </>
   );
-};
+}
 
 export default PlayerLayout;
