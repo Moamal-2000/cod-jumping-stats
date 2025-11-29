5145ed2986f27ca12ba780036fde6fb3705ff9bc
Moamal-2000 <moamalalaapro1@gmail.com>
2025-11-29 08:13:05 +0300

    chore(TopRunsTBody): add spacing between <td> elements for better readability

--- frontend/src/components/Pages/PlayerProfile/TopRuns/TopRunsTable/TopRunsTBody/TopRunsTBody.jsx
+++ frontend/src/components/Pages/PlayerProfile/TopRuns/TopRunsTable/TopRunsTBody/TopRunsTBody.jsx
@@ -14,16 +14,21 @@ const TopRunsTBody = ({ processedRuns }) => {
                 <span>{run.totalNr}</span>
               </div>
             </td>
+
             <td className={s.mapNameCell} title={run.mapname}>
               {run.mapname}
             </td>
+
             <td>
               <span className={s.fpsCell}>{run.fps}</span>
             </td>
+
             <td>
               <span className={s.nadesCell}>{run.nadejumps}</span>
             </td>
+
             <td className={s.timeCell}>{formatTime(run.time_played_string)}</td>
+
             <td className={s.dateCell}>
               {formatDate(run.time_created, "Unknown")}
             </td>
