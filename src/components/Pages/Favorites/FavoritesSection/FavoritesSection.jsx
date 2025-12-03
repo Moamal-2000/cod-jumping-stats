a485dcd65f2e0708abb3343505f85ed99388266b
Moamal-2000 <moamalalaapro1@gmail.com>
2025-12-03 12:58:18 +0300

    fix(FavoritesSection): access localStorage on client and show message when no favorite maps exist

--- src/components/Pages/Favorites/FavoritesSection/FavoritesSection.jsx
+++ src/components/Pages/Favorites/FavoritesSection/FavoritesSection.jsx
@@ -1,29 +1,40 @@
 "use client";
 
 import { fetchMaps } from "@/redux/features/maps/thunk/mapsThunk";
-import { useEffect } from "react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import MapCard from "../../Maps/MapCard/MapCard";
 import s from "./FavoritesSection.module.scss";
 
 const FavoritesSection = () => {
-  const favoritesLocal = localStorage.getItem("favorites");
+  const { allMaps } = useSelector((s) => s.maps);
+  const [favMaps, setFavMaps] = useState([]);
 
   const dispatch = useDispatch();
-  const { allMaps } = useSelector((s) => s.maps);
-
-  if (!favoritesLocal) return null;
-
-  const favorites = JSON.parse(favoritesLocal);
-  const favMaps = allMaps.filter((map) => favorites.mapsIds.includes(map.CpID));
 
   useEffect(() => {
     if (allMaps.length <= 0) dispatch(fetchMaps());
-  }, []);
+
+    const favoritesLocal = localStorage.getItem("favorites");
+    const favorites = JSON.parse(favoritesLocal);
+
+    const filteredFavMaps = allMaps.filter((map) =>
+      favorites.mapsIds.includes(map.CpID)
+    );
+
+    setFavMaps(filteredFavMaps);
+  }, [allMaps]);
 
   return (
     <div className="container">
       <section className={s.mapsSection}>
+        {favMaps.length === 0 && (
+          <p>
+            You have no favorite <Link href="/maps">maps</Link>, add some.
+          </p>
+        )}
+
         {favMaps.map((map, index) => (
           <MapCard
             key={map.CpID}
