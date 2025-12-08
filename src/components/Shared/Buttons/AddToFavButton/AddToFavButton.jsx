b1293d2b63906b06e89860ef87eb2dd1687df632
Moamal-2000 <moamalalaapro1@gmail.com>
2025-12-08 13:47:17 +0300

    feat(AddToFavButton): add JSX elements, interaction logic, and styles

--- src/components/Shared/Buttons/AddToFavButton/AddToFavButton.jsx
+++ src/components/Shared/Buttons/AddToFavButton/AddToFavButton.jsx
@@ -1,9 +1,31 @@
-import s from './AddToFavButton.module.scss'
+"use client";
 
-const AddToFavButton = () => {
+import { checkIsItemFavorited, toggleFavorite } from "@/functions/components";
+import { useEffect, useState } from "react";
+import s from "./AddToFavButton.module.scss";
+
+const AddToFavButton = ({ id, groupKey }) => {
+  const [isFavorited, setIsFavorited] = useState(false);
+
+  useEffect(() => {
+    checkIsItemFavorited({ setIsFavorited, id, groupKey });
+  }, [id]);
+
   return (
-    <div>AddToFavButton</div>
-  )
-}
+    <button
+      type="button"
+      onClick={() => toggleFavorite({ setIsFavorited, id, groupKey })}
+      className={`${s.favoriteButton} ${isFavorited ? s.favorited : ""}`}
+      title={isFavorited ? "Remove from favorites" : "Add to favorites"}
+      aria-label={isFavorited ? "Remove from favorites" : "Add to favorites"}
+    >
+      <svg viewBox="0 0 24 24">
+        <use
+          href={`/icons-sprite.svg#${isFavorited ? "trashCan" : "heart"}`}
+        ></use>
+      </svg>
+    </button>
+  );
+};
 
-export default AddToFavButton
+export default AddToFavButton;
