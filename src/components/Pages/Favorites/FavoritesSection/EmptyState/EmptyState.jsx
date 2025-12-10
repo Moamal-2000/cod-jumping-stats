36a4eb77c6f6530a96308d45083dea4dd09c31b7
Moamal-2000 <moamalalaapro1@gmail.com>
2025-12-11 01:06:11 +0300

    style(EmptyState): rename ctaButton class name to browseLink and update link styling

--- src/components/Pages/Favorites/FavoritesSection/EmptyState/EmptyState.jsx
+++ src/components/Pages/Favorites/FavoritesSection/EmptyState/EmptyState.jsx
@@ -15,7 +15,7 @@ const EmptyState = ({ type }) => {
       <h3 className={s.title}>{title}</h3>
       <p className={s.description}>{description}</p>
 
-      <Link href={linkHref} className={s.ctaButton}>
+      <Link href={linkHref} className={s.browseLink}>
         <svg className={s.ctaIcon} aria-hidden="true">
           <use href={`/icons-sprite.svg#${icon}`} />
         </svg>
