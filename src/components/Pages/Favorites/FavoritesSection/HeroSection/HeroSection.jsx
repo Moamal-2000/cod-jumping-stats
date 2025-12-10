16c73f5180ad9e2383be211c497e53c059431e05
Moamal-2000 <moamalalaapro1@gmail.com>
2025-12-11 00:47:08 +0300

    chore(favorites-page): remove unnecessary <div> wrapper from HeroSection

--- src/components/Pages/Favorites/FavoritesSection/HeroSection/HeroSection.jsx
+++ src/components/Pages/Favorites/FavoritesSection/HeroSection/HeroSection.jsx
@@ -3,20 +3,18 @@ import s from "./HeroSection.module.scss";
 const HeroSection = () => {
   return (
     <section className={s.hero}>
-      <div className={s.content}>
-        <div className={s.titleWrapper}>
-          <h1 className={s.title}>Your Favorites</h1>
+      <div className={s.titleWrapper}>
+        <h1 className={s.title}>Your Favorites</h1>
 
-          <svg className={s.icon} aria-hidden="true">
-            <use href="/icons-sprite.svg#heart" />
-          </svg>
-        </div>
-
-        <p className={s.description}>
-          Keep track of your favorite maps and players all in one place. Quick
-          access to everything you love.
-        </p>
+        <svg className={s.icon} aria-hidden="true">
+          <use href="/icons-sprite.svg#heart" />
+        </svg>
       </div>
+
+      <p className={s.description}>
+        Keep track of your favorite maps and players all in one place. Quick
+        access to everything you love.
+      </p>
     </section>
   );
 };
