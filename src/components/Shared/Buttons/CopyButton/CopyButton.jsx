1b2b1fb368a145bdeb4e7cd71fc46b673371c30e
Moamal-2000 <moamalalaapro1@gmail.com>
2025-12-16 06:17:22 +0300

    refactor(CopyButton): move handleClick outside component to prevent function recreation

--- src/components/Shared/Buttons/CopyButton/CopyButton.jsx
+++ src/components/Shared/Buttons/CopyButton/CopyButton.jsx
@@ -7,26 +7,12 @@ import s from "./CopyButton.module.scss";
 const CopyButton = ({ copyText, title }) => {
   const [isCopied, setIsCopied] = useState(false);
 
-  async function handleClick() {
-    if (isCopied) return;
-
-    try {
-      navigator.clipboard.writeText(copyText);
-      setIsCopied(true);
-
-      setTimeout(() => setIsCopied(false), COPY_BUTTON_DELAY_MS);
-    } catch (err) {
-      console.error("Can't copy text:", err);
-      setIsCopied(false);
-    }
-  }
-
   return (
     <button
       className={s.copyButton}
       title={title}
       type="button"
-      onClick={handleClick}
+      onClick={() => handleClick({ copyText, isCopied, setIsCopied })}
     >
       <svg aria-hidden="true">
         <use href={`icons-sprite.svg#${isCopied ? "checked" : "copy"}`} />
@@ -36,3 +22,17 @@ const CopyButton = ({ copyText, title }) => {
 };
 
 export default CopyButton;
+
+async function handleClick({ copyText, isCopied, setIsCopied } = {}) {
+  if (isCopied) return;
+
+  try {
+    navigator.clipboard.writeText(copyText);
+    setIsCopied(true);
+
+    setTimeout(() => setIsCopied(false), COPY_BUTTON_DELAY_MS);
+  } catch (err) {
+    console.error("Can't copy text:", err);
+    setIsCopied(false);
+  }
+}
