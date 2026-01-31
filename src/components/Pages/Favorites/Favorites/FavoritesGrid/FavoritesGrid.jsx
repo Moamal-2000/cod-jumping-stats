00ad75ad50ac8c936846160cd4dae861bdaf02a5
Moamal-2000 <moamalalaapro1@gmail.com>
2026-01-31 13:42:54 +0300

    remove(component): delete unused FavoritesGrid component

--- src/components/Pages/Favorites/Favorites/FavoritesGrid/FavoritesGrid.jsx
+++
@@ -1,7 +0,0 @@
-import s from "./FavoritesGrid.module.scss";
-
-const FavoritesGrid = ({ children, variant = "maps" }) => {
-  return <div className={`${s.grid} ${s[variant]}`}>{children}</div>;
-};
-
-export default FavoritesGrid;
