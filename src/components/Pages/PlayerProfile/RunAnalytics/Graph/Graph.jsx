6e979d28366e0629427f53ae213d7e37d5a965ad
Moamal-2000 <moamalalaapro1@gmail.com>
2026-02-13 15:34:26 +0300

    refactor(Graph): extract loading spinner into LoadingSpinner component

--- src/components/Pages/PlayerProfile/RunAnalytics/Graph/Graph.jsx
+++ src/components/Pages/PlayerProfile/RunAnalytics/Graph/Graph.jsx
@@ -1,31 +1,8 @@
 "use client";
 import { getColoredName } from "@/functions/components";
 import { useEffect, useRef, useState } from "react";
-import s from "./Graph.module.scss"
-
-const LoadingSpinner = () => (
-  <svg
-    width="48"
-    height="48"
-    viewBox="0 0 24 24"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2Z"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeOpacity="0.2"
-    />
-    <path
-      d="M12 2C6.48 2 2 6.48 2 12C2 14.9 3.26 17.46 5.25 19.19L12 12.44V2Z"
-      fill="currentColor"
-      className={s.spinnerPath}
-    />
-  </svg>
-);
+import s from "./Graph.module.scss";
+import LoadingSpinner from "./LoadingSpinner/LoadingSpinner";
 
 // Icons
 const ZoomInIcon = () => (
---
+++ src/components/Pages/PlayerProfile/RunAnalytics/Graph/LoadingSpinner/LoadingSpinner.jsx
@@ -0,0 +1,29 @@
+"use client";
+
+import s from "./LoadingSpinner.module.scss";
+
+const LoadingSpinner = () => (
+  <svg
+    className={s.spinner}
+    width="48"
+    height="48"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2Z"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeOpacity="0.2"
+    />
+    <path
+      d="M12 2C6.48 2 2 6.48 2 12C2 14.9 3.26 17.46 5.25 19.19L12 12.44V2Z"
+      fill="currentColor"
+    />
+  </svg>
+);
+
+export default LoadingSpinner;
