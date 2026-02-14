b1777c498a2aa67d36114f58b5d1437b6245df3c
Moamal-2000 <moamalalaapro1@gmail.com>
2026-02-14 21:10:57 +0300

    init: XAxisLabels and YAxisLabels components

---
+++ src/components/Pages/PlayerProfile/RunAnalytics/Graph/XAxisLabels/XAxisLabels.jsx
@@ -0,0 +1,9 @@
+import s from './XAxisLabels.module.scss'
+
+const XAxisLabels = () => {
+  return (
+    <div>XAxisLabels</div>
+  )
+}
+
+export default XAxisLabels
---
+++ src/components/Pages/PlayerProfile/RunAnalytics/Graph/YAxisLabels/YAxisLabels.jsx
@@ -0,0 +1,9 @@
+import s from './YAxisLabels.module.scss'
+
+const YAxisLabels = () => {
+  return (
+    <div>YAxisLabels</div>
+  )
+}
+
+export default YAxisLabels
