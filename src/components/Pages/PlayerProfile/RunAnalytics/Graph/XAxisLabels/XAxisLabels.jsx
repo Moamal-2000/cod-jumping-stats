6c52d6284b9819771c9d80c00274991dd00305ad
Moamal-2000 <moamalalaapro1@gmail.com>
2026-02-14 21:37:26 +0300

    refactor(XAxisLabels): move run years extraction logic to getGraphRunYears function outside component

--- src/components/Pages/PlayerProfile/RunAnalytics/Graph/XAxisLabels/XAxisLabels.jsx
+++ src/components/Pages/PlayerProfile/RunAnalytics/Graph/XAxisLabels/XAxisLabels.jsx
@@ -2,16 +2,7 @@ import { CHART_HEIGHT } from "@/data/constants";
 import s from "./XAxisLabels.module.scss";
 
 const XAxisLabels = ({ allTimestamps, scaleTimestampToX }) => {
-  const minTimestamp = Math.min(...allTimestamps);
-  const maxTimestamp = Math.max(...allTimestamps);
-
-  const minYear = new Date(minTimestamp).getFullYear();
-  const maxYear = new Date(maxTimestamp).getFullYear();
-
-  const years = Array.from(
-    { length: maxYear - minYear + 1 },
-    (_, i) => minYear + i,
-  );
+  const years = getGraphRunYears(allTimestamps);
 
   return years.map((year) => {
     const yearStartTimestamp = new Date(year, 0, 1).getTime();
@@ -32,3 +23,13 @@ const XAxisLabels = ({ allTimestamps, scaleTimestampToX }) => {
 };
 
 export default XAxisLabels;
+
+function getGraphRunYears(allTimestamps) {
+  const minTimestamp = Math.min(...allTimestamps);
+  const maxTimestamp = Math.max(...allTimestamps);
+
+  const minYear = new Date(minTimestamp).getFullYear();
+  const maxYear = new Date(maxTimestamp).getFullYear();
+
+  return Array.from({ length: maxYear - minYear + 1 }, (_, i) => minYear + i);
+}
