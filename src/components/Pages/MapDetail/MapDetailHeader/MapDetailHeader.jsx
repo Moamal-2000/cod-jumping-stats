8feb92da445768535fec5f60302c74804046f2a7
Moamal-2000 <moamalalaapro1@gmail.com>
2026-02-15 05:36:29 +0300

    feat(MapDetailHeader): display best player name in map details

--- src/components/Pages/MapDetail/MapDetailHeader/MapDetailHeader.jsx
+++ src/components/Pages/MapDetail/MapDetailHeader/MapDetailHeader.jsx
@@ -1,20 +1,91 @@
+import { jhApis } from "@/api/jumpersHeaven";
 import MapImage from "@/components/Shared/Images/MapImage/MapImage";
 import MapRoutesSelector from "@/components/Shared/MapRoutesSelector/MapRoutesSelector";
+import { JUMP_FPS } from "@/data/constants";
+import { getColoredName } from "@/functions/components";
 import { formateReleaseDate } from "@/functions/utils";
 import { fetchMaps } from "@/redux/features/maps/thunk/mapsThunk";
-import { useEffect } from "react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import s from "./MapDetailHeader.module.scss";
 
 const MapDetailHeader = ({ mapData }) => {
-  const { Name, Author, Released, Type, Ender } = mapData;
+  const { Name, Author, Released, Type, Ender, CpID } = mapData;
+  const [bestPlayer, setBestPlayer] = useState(null);
 
-  const allMaps = useSelector((s) => s.maps.allMaps);
+  const allMaps = useSelector((state) => state.maps.allMaps);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if (allMaps.length <= 0) dispatch(fetchMaps());
-  }, []);
+    if (allMaps.length <= 0) {
+      dispatch(fetchMaps());
+    }
+  }, [allMaps.length, dispatch]);
+
+  useEffect(() => {
+    if (!CpID) {
+      setBestPlayer(null);
+      return;
+    }
+
+    let cancelled = false;
+    setBestPlayer(null);
+
+    async function fetchBestPlayer() {
+      const results = await Promise.all(
+        JUMP_FPS.map((fps) =>
+          fetch(jhApis({ fps, cpid: CpID, limit: 50 }).map.tops)
+            .then((res) => res.json())
+            .then((data) => {
+              if (Array.isArray(data)) {
+                return data.map((run) => ({ ...run, fps }));
+              }
+              return [];
+            })
+            .catch(() => []),
+        ),
+      );
+
+      if (cancelled) {
+        return;
+      }
+
+      const allRuns = results
+        .filter((result) => Array.isArray(result))
+        .flat()
+        .filter(
+          (item) =>
+            item &&
+            typeof item === "object" &&
+            item.time_played !== null &&
+            item.time_played !== undefined,
+        )
+        .sort((a, b) => a.time_played - b.time_played);
+
+      if (allRuns.length === 0) {
+        setBestPlayer(null);
+        return;
+      }
+
+      const bestPlayerRun = allRuns[0];
+
+      setBestPlayer(
+        bestPlayerRun
+          ? {
+              playerId: bestPlayerRun.player_id,
+              playerName: bestPlayerRun.playername,
+            }
+          : null,
+      );
+    }
+
+    fetchBestPlayer();
+
+    return () => {
+      cancelled = true;
+    };
+  }, [CpID]);
 
   return (
     <div className={s.header}>
@@ -44,6 +115,22 @@ const MapDetailHeader = ({ mapData }) => {
                 {Type}
               </span>
             </div>
+
+            {bestPlayer?.playerName && (
+              <div className={s.metaItem}>
+                <span className={s.label}>Best player:</span>
+                <span className={`${s.value} ${s.bestPlayerValue}`}>
+                  {bestPlayer.playerId ? (
+                    <Link href={`/player?playerid=${bestPlayer.playerId}`}>
+                      {getColoredName(bestPlayer.playerName)}
+                    </Link>
+                  ) : (
+                    bestPlayer.playerName
+                  )}{" "}
+                  is the best on this map
+                </span>
+              </div>
+            )}
           </div>
         </div>
 
