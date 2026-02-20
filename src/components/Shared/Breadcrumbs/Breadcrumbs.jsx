81c7b22abf268c38989edef75b766d0513df278d
Moamal-2000 <moamalalaapro1@gmail.com>
2026-02-21 02:49:04 +0300

    style(PlayerProfileLayout): align Breadcrumbs to left on mobile and add data-type attribute for external styling control

--- src/components/Shared/Breadcrumbs/Breadcrumbs.jsx
+++ src/components/Shared/Breadcrumbs/Breadcrumbs.jsx
@@ -6,7 +6,7 @@ const Breadcrumbs = ({ breadcrumbLabels, breadcrumbPaths }) => {
   const currentPage = breadcrumbLabels.at(-1);
 
   return (
-    <div className={s.breadcrumbs}>
+    <div className={s.breadcrumbs} data-type="breadcrumbs">
       {previousPages.map((page, index) => (
         <div className={s.page} key={index}>
           <Link href={breadcrumbPaths?.[index]?.path}>{page}</Link>
