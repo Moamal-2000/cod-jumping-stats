40741bdf5de5479bb0d7fe8df9f8f748355fb59e
Moamal-2000 <moamalalaapro1@gmail.com>
2026-02-21 08:33:08 +0300

    refactor(PlayersSection): simplify component y extracting players table into PlayersTable component

--- src/components/Pages/PlayersPage/PlayersSection/PlayersTable/PlayersTable.jsx
+++ src/components/Pages/PlayersPage/PlayersSection/PlayersTable/PlayersTable.jsx
@@ -1,9 +1,75 @@
-import s from './PlayersTable.module.scss'
+import CountryImage from "@/components/Shared/Images/CountryImage/CountryImage";
+import { getColoredName } from "@/functions/components";
+import Link from "next/link";
+import PlayerBadges from "../../PlayerCard/PlayerBadges/PlayerBadges";
+import { formatLastSeen } from "../../PlayerCard/PlayerStats/PlayerStats";
+import s from "./PlayersTable.module.scss";
 
-const PlayersTable = () => {
+const PlayersTable = ({ playersScroll, lastPlayerRef }) => {
   return (
-    <div>PlayersTable</div>
-  )
-}
+    <table className={s.playersTable}>
+      <thead>
+        <tr>
+          <th>Player</th>
+          <th>Visits</th>
+          <th>Last Seen</th>
+          <th>Badges</th>
+        </tr>
+      </thead>
 
-export default PlayersTable
+      <tbody>
+        {playersScroll.map((player, index) => {
+          const ref = playersScroll.length === index + 1 ? lastPlayerRef : null;
+
+          return (
+            <tr key={player.PlayerID} ref={ref}>
+              <td className={s.playerNameCell}>
+                <div className={s.playerInfoWrapper}>
+                  <div className={s.countryFlag}>
+                    <CountryImage
+                      countryCode={player.Country}
+                      size={32}
+                      colorPlaceholder={true}
+                    />
+                  </div>
+                  <Link
+                    href={`/player/${player.PlayerID}`}
+                    className={s.playerLink}
+                  >
+                    <div className={s.playerNameWrapper}>
+                      <span className={s.playerName}>
+                        {getColoredName(player.PlayerName || player.PrefName)}
+                      </span>
+                      {player.PrefName && (
+                        <span className={s.originalName}>
+                          {getColoredName(player.PrefName)}
+                        </span>
+                      )}
+                    </div>
+                  </Link>
+                </div>
+              </td>
+              <td className={s.visitsCell}>
+                <span className={s.visitsValue}>{player.Visits || 0}</span>
+              </td>
+              <td className={s.lastSeenCell}>
+                {formatLastSeen(player.LastSeen) || "Never"}
+              </td>
+              <td className={s.badgesCell}>
+                <PlayerBadges
+                  Admin={player.Admin}
+                  Banned={player.Banned}
+                  Donated={player.Donated}
+                  PlayerID={player.PlayerID}
+                  LastSeen={player.LastSeen}
+                />
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default PlayersTable;
