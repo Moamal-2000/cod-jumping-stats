a5820b83c6ecd80b578de0085c36d39120213867
Moamal-2000 <moamalalaapro1@gmail.com>
2026-02-22 08:52:48 +0300

    feat(metadata): add site keywords for SEO optimization in metadata.js

--- src/data/metadata.js
+++ src/data/metadata.js
@@ -1,7 +1,27 @@
 export const BASE_URL = process.env.SITE_MAP;
 const title = "Servers | JumpersHeaven";
 
+const keywords = [
+  "Jumpers Heaven",
+  "Jumpers Heaven Stats",
+  "Jumpers Heaven Leaderboards",
+  "Jumpers Heaven Players",
+  "Jumpers Heaven Maps",
+  "JH Stats",
+  "JH Leaderboards",
+  "Call of Duty 2 Jumpers Heaven",
+  "CoD2 Jumpers Heaven",
+  "CoD2 Jumper Stats",
+  "Call of Duty 2 Leaderboards",
+  "Call of Duty 2 Player Stats",
+  "CoD2 Records",
+  "CoD2 Maps",
+  "Jumper Mod CoD2",
+  "Jumping Mod Call of Duty 2",
+  "Game Server Statistics",
+];
+
 const description =
   "Browse active JumpersHeaven mod servers, monitor live player counts, and quickly jump to server-specific activity.";
 
-export const METADATA = { title, description };
+export const METADATA = { title, description, keywords };
