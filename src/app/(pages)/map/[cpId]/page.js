560cf0776355b670cac1ef6b28c3415eebab19dd
Moamal-2000 <moamalalaapro1@gmail.com>
2026-02-24 06:02:27 +0300

    feat(metadata): generate dynamic metadata for individual maps

--- src/app/(pages)/map/[cpId]/page.js
+++ src/app/(pages)/map/[cpId]/page.js
@@ -1,6 +1,36 @@
 import MapDetailPage from "@/components/Pages/MapDetail/MapDetailPage";
+import { getOpenGraphMetadata, SITE_URL } from "@/data/metadata";
+import { getMapByCpId } from "@/functions/utils";
+import { size } from "./opengraph-image";
 
 export const revalidate = 86400; // 1 day
+
+export async function generateMetadata({ params }) {
+  const { cpId } = await params;
+  const map = await getMapByCpId(cpId);
+
+  const mapName = map.Name;
+
+  const title = `${mapName} Map | JumpersHeaven`;
+  const description = `View JumpersHeaven map details and statistics for ${mapName}`;
+
+  const generatedMetadata = {
+    title,
+    description,
+    metadataBase: new URL(SITE_URL),
+    ...getOpenGraphMetadata({
+      title,
+      description,
+      imageUrl: `${SITE_URL}/map/${cpId}/opengraph-image`,
+      imageAlt: `Jumpers Heaven ${mapName} Map`,
+      pagePath: `map/${cpId}`,
+      imageType: "image/png",
+      imageSize: size,
+    }),
+  };
+
+  return generatedMetadata;
+}
 
 async function MapPage({ params }) {
   const { cpId } = await params;
