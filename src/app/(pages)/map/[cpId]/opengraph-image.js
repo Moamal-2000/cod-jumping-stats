0ba44ebe8a5ea42fafbd6e970f40d7c31076c116
Moamal-2000 <moamalalaapro1@gmail.com>
2026-02-24 05:50:19 +0300

    feat(openGraph): add fallback text for missing map images

--- src/app/(pages)/map/[cpId]/opengraph-image.js
+++ src/app/(pages)/map/[cpId]/opengraph-image.js
@@ -14,6 +14,8 @@ export default async function Image({ params }) {
 
   const MapOpenGraphElement = (
     <div style={s.container}>
+      <p style={s.noImageText}>No Image Available for Map {map.Name}</p>
+
       <div style={s.mapContainer}>
         <img src={mapImagePath} width="100%" height="100%" style={s.mapImage} />
         <span style={s.mapName}>By: {map.Author}</span>
@@ -29,8 +31,23 @@ const s = {
     width: "100%",
     height: "100%",
     display: "flex",
-    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
     background: "linear-gradient(135deg, #020617, #0f172a)",
+  },
+
+  noImageText: {
+    fontSize: 60,
+    fontWeight: 600,
+    color: "white",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    position: "absolute",
+    textWrap: "wrap",
+    textAlign: "center",
+    width: size.width - 100,
+    padding: 20,
+    right: 50,
+    zIndex: -1,
   },
 
   mapContainer: {
@@ -44,6 +61,9 @@ const s = {
     height: size.height,
     filter: "brightness(1.2)",
     objectFit: "cover",
+    position: "absolute",
+    top: 0,
+    left: 0,
   },
 
   mapName: {
@@ -52,6 +72,7 @@ const s = {
     color: "white",
     position: "absolute",
     backgroundColor: "rgba(0, 0, 0, 0.5)",
+    padding: 20,
     bottom: 50,
     left: 50,
   },
