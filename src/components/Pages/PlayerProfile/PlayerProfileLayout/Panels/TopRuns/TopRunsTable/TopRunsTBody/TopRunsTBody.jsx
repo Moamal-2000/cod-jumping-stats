f4707bde641c2af0f795a0c2ea4aebfd6a3e9087
Moamal-2000 <moamalalaapro1@gmail.com>
2026-03-13 17:58:39 +0300

    style(TopRunsTBody): use card layout for rows on mobile screens

--- src/components/Pages/PlayerProfile/PlayerProfileLayout/Panels/TopRuns/TopRunsTable/TopRunsTBody/TopRunsTBody.jsx
+++ src/components/Pages/PlayerProfile/PlayerProfileLayout/Panels/TopRuns/TopRunsTable/TopRunsTBody/TopRunsTBody.jsx
@@ -8,20 +8,28 @@ const TopRunsTBody = ({ topRuns }) => {
       {topRuns.map((run, index) => {
         return (
           <tr key={`${run.RunID}-${index}`}>
-            <td className={s.rankCell}>
+            <td className={s.rankCell} data-header="Rank">
               {run.Rank}/{run.TotalNr}
             </td>
 
-            <td className={s.mapNameCell}>
+            <td className={s.mapNameCell} data-header="Map">
               <Link href={`/map/${run.CpID}`}>{run.MapName}</Link>
             </td>
 
-            <td className={s.fpsCell}>{run.FPS === "0" ? "Mix" : run.FPS}</td>
-            <td className={s.scoreCell}>{run?.Score}</td>
-            <td className={s.nadesCell}>{run.Nadejumps}</td>
-            <td className={s.timeCell}>{formatTime(run.TimePlayedString)}</td>
+            <td className={s.fpsCell} data-header="FPS">
+              {run.FPS === "0" ? "Mix" : run.FPS}
+            </td>
+            <td className={s.scoreCell} data-header="Skill Points">
+              {run?.Score}
+            </td>
+            <td className={s.nadesCell} data-header="Nade Jumps">
+              {run.Nadejumps}
+            </td>
+            <td className={s.timeCell} data-header="Time">
+              {formatTime(run.TimePlayedString)}
+            </td>
 
-            <td className={s.dateCell}>
+            <td className={s.dateCell} data-header="Date">
               {formatDate(run.TimeCreated, "Unknown")}
             </td>
           </tr>
