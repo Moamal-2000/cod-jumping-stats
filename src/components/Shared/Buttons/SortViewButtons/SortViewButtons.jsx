15f5a0940984ca030a59d62acaa9ad4f6bed5780
Moamal-2000 <moamalalaapro1@gmail.com>
2026-03-19 02:24:25 +0300

    feat(SortViewButtons): build the structure, logic and styles and support optional props

--- src/components/Shared/Buttons/SortViewButtons/SortViewButtons.jsx
+++ src/components/Shared/Buttons/SortViewButtons/SortViewButtons.jsx
@@ -1,8 +1,50 @@
-import s from "./SortViewButtons.module.scss"
+"use client";
 
-const SortViewButtons = () => {
+import { VIEW_OPTIONS_DATA } from "@/data/constants";
+import { createQueryString, removeQueryString } from "@/lib/queryParams";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import s from "./SortViewButtons.module.scss";
+
+const SortViewButtons = ({ defaultView = "grid", queryName = "view" }) => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const viewBy = searchParams.get(queryName) || defaultView;
+
+  function changeView(value) {
+    const isDefault = value === defaultView;
+
+    if (isDefault) {
+      removeQueryString(queryName, searchParams, router, pathname);
+      return;
+    }
+
+    createQueryString(queryName, value, searchParams, router, pathname);
+  }
+
   return (
-    <div>SortViewButtons</div>
-  )
-}
-export default SortViewButtons
+    <div className={s.buttons}>
+      {VIEW_OPTIONS_DATA.map(({ value, icon, id }) => {
+        const activeClass = viewBy === value ? s.active : "";
+        const title = `Change view to ${value}`;
+
+        return (
+          <button
+            key={id}
+            type="button"
+            className={`${s.viewButton} ${activeClass}`}
+            onClick={() => changeView(value)}
+            title={title}
+            aria-label={title}
+          >
+            <svg aria-hidden="true">
+              <use href={`/icons-sprite.svg#${icon}`} />
+            </svg>
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+export default SortViewButtons;
