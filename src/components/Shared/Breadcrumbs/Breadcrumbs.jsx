6480529a8f0770e47fa541a3316cc5e48a2d4e2e
Moamal-2000 <moamalalaapro1@gmail.com>
2026-03-30 14:06:56 +0300

    accessibility(Breadcrumbs): replace <div> semantic <nav> element

--- src/components/Shared/Breadcrumbs/Breadcrumbs.jsx
+++ src/components/Shared/Breadcrumbs/Breadcrumbs.jsx
@@ -6,7 +6,7 @@ const Breadcrumbs = ({ breadcrumbLabels, breadcrumbPaths }) => {
   const currentPage = breadcrumbLabels.at(-1);
 
   return (
-    <div className={s.breadcrumbs} data-type="breadcrumbs">
+    <nav className={s.breadcrumbs} data-type="breadcrumbs">
       {previousPages.map((page, index) => (
         <div className={s.page} key={index}>
           <Link href={breadcrumbPaths?.[index]?.path}>{page}</Link>
@@ -15,7 +15,7 @@ const Breadcrumbs = ({ breadcrumbLabels, breadcrumbPaths }) => {
       ))}
 
       <span className={s.currentPage}>{currentPage || "Unknown"}</span>
-    </div>
+    </nav>
   );
 };
 export default Breadcrumbs;
