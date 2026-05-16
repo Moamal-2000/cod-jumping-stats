8e76d75861272237a32732e061ef9d629b35cfb7
Moamal-2000 <moamalalaapro1@gmail.com>
2026-05-16 20:36:36 +0300

    refactor(PlayersSection): extract badge statistics UI into BadgeStats component

--- src/components/Pages/PlayersPage/PlayersSection/BadgeStats/BadgeStats.jsx
+++ src/components/Pages/PlayersPage/PlayersSection/BadgeStats/BadgeStats.jsx
@@ -1,8 +1,60 @@
-import s from "./BadgeStats.module.scss"
+import {
+  bugHunterIds,
+  contentCreators,
+  eventWinnerIds,
+  mappersIds,
+} from "@/data/manualBadges";
+import { isActiveWithinWeek } from "@/lib/validation";
+import s from "./BadgeStats.module.scss";
 
-const BadgeStats = () => {
+const BadgeStats = ({ playersData }) => {
+  const badgesCount = {
+    winner: eventWinnerIds.length,
+    bugHunter: bugHunterIds.length,
+    mapper: mappersIds.length,
+    creator: contentCreators.length,
+    banned: playersData.filter((player) => player.Banned === 1).length,
+    donator: playersData.filter((player) => player.Donated === 1).length,
+    active: playersData.filter((player) => isActiveWithinWeek(player.LastSeen))
+      .length,
+    admin: playersData.filter((player) => player.Admin >= 100).length,
+  };
+
   return (
-    <div>BadgeStats</div>
-  )
-}
-export default BadgeStats
+    <div className={s.badgesCountContainer}>
+      <div className={s.badgeCountItem}>
+        <span className={s.badgeCountLabel}>Event Winners</span>
+        <span className={s.badgeCountValue}>{badgesCount.winner}</span>
+      </div>
+      <div className={s.badgeCountItem}>
+        <span className={s.badgeCountLabel}>Bug Hunters</span>
+        <span className={s.badgeCountValue}>{badgesCount.bugHunter}</span>
+      </div>
+      <div className={s.badgeCountItem}>
+        <span className={s.badgeCountLabel}>Mappers</span>
+        <span className={s.badgeCountValue}>{badgesCount.mapper}</span>
+      </div>
+      <div className={s.badgeCountItem}>
+        <span className={s.badgeCountLabel}>Content Creators</span>
+        <span className={s.badgeCountValue}>{badgesCount.creator}</span>
+      </div>
+      <div className={s.badgeCountItem}>
+        <span className={s.badgeCountLabel}>Banned</span>
+        <span className={s.badgeCountValue}>{badgesCount.banned}</span>
+      </div>
+      <div className={s.badgeCountItem}>
+        <span className={s.badgeCountLabel}>Donators</span>
+        <span className={s.badgeCountValue}>{badgesCount.donator}</span>
+      </div>
+      <div className={s.badgeCountItem}>
+        <span className={s.badgeCountLabel}>Active</span>
+        <span className={s.badgeCountValue}>{badgesCount.active}</span>
+      </div>
+      <div className={s.badgeCountItem}>
+        <span className={s.badgeCountLabel}>Admins</span>
+        <span className={s.badgeCountValue}>{badgesCount.admin}</span>
+      </div>
+    </div>
+  );
+};
+export default BadgeStats;
--- src/components/Pages/PlayersPage/PlayersSection/PlayersSection.jsx
+++ src/components/Pages/PlayersPage/PlayersSection/PlayersSection.jsx
@@ -1,15 +1,9 @@
 "use client";
 
 import { DEFAULT_VIEW_MODE } from "@/data/constants";
-import {
-  bugHunterIds,
-  contentCreators,
-  eventWinnerIds,
-  mappersIds,
-} from "@/data/manualBadges";
-import { isActiveWithinWeek } from "@/lib/validation";
 import { useSearchParams } from "next/navigation";
 import PlayerCard from "../PlayerCard/PlayerCard";
+import BadgeStats from "./BadgeStats/BadgeStats";
 import s from "./PlayersSection.module.scss";
 import PlayersTable from "./PlayersTable/PlayersTable";
 
@@ -23,54 +17,9 @@ const PlayersSection = ({
   const searchParams = useSearchParams();
   const viewType = searchParams.get("view") || DEFAULT_VIEW_MODE;
 
-  const badgesCount = {
-    winner: eventWinnerIds.length,
-    bugHunter: bugHunterIds.length,
-    mapper: mappersIds.length,
-    creator: contentCreators.length,
-    banned: playersData.filter((player) => player.Banned === 1).length,
-    donator: playersData.filter((player) => player.Donated === 1).length,
-    active: playersData.filter((player) => isActiveWithinWeek(player.LastSeen))
-      .length,
-    admin: playersData.filter((player) => player.Admin >= 100).length,
-  };
-
   return (
     <section className={s.playersSection}>
-      <div className={s.badgesCountContainer}>
-        <div className={s.badgeCountItem}>
-          <span className={s.badgeCountLabel}>Event Winners</span>
-          <span className={s.badgeCountValue}>{badgesCount.winner}</span>
-        </div>
-        <div className={s.badgeCountItem}>
-          <span className={s.badgeCountLabel}>Bug Hunters</span>
-          <span className={s.badgeCountValue}>{badgesCount.bugHunter}</span>
-        </div>
-        <div className={s.badgeCountItem}>
-          <span className={s.badgeCountLabel}>Mappers</span>
-          <span className={s.badgeCountValue}>{badgesCount.mapper}</span>
-        </div>
-        <div className={s.badgeCountItem}>
-          <span className={s.badgeCountLabel}>Content Creators</span>
-          <span className={s.badgeCountValue}>{badgesCount.creator}</span>
-        </div>
-        <div className={s.badgeCountItem}>
-          <span className={s.badgeCountLabel}>Banned</span>
-          <span className={s.badgeCountValue}>{badgesCount.banned}</span>
-        </div>
-        <div className={s.badgeCountItem}>
-          <span className={s.badgeCountLabel}>Donators</span>
-          <span className={s.badgeCountValue}>{badgesCount.donator}</span>
-        </div>
-        <div className={s.badgeCountItem}>
-          <span className={s.badgeCountLabel}>Active</span>
-          <span className={s.badgeCountValue}>{badgesCount.active}</span>
-        </div>
-        <div className={s.badgeCountItem}>
-          <span className={s.badgeCountLabel}>Admins</span>
-          <span className={s.badgeCountValue}>{badgesCount.admin}</span>
-        </div>
-      </div>
+      <BadgeStats playersData={playersData} />
 
       {viewType === "grid" && (
         <div className={s.playersGrid}>
