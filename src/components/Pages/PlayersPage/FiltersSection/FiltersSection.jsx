4122ff888eb773062bb841edff04beb5c1405cb4
Moamal-2000 <moamalalaapro1@gmail.com>
2026-05-16 21:12:51 +0300

    chore(PlayersPage): remove badges selector from FiltersSection

--- src/components/Pages/PlayersPage/FiltersSection/FiltersSection.jsx
+++ src/components/Pages/PlayersPage/FiltersSection/FiltersSection.jsx
@@ -2,9 +2,8 @@
 
 import SortViewButtons from "@/components/Shared/Buttons/SortViewButtons/SortViewButtons";
 import SearchInput from "@/components/Shared/Inputs/SearchInput/SearchInput";
-import SelectMenu from "@/components/Shared/SelectMenus/SelectMenu/SelectMenu";
 import ResultsSummary from "@/components/Shared/Texts/ResultsSummary/ResultsSummary";
-import { FILTER_PLAYERS_BADGES, SORT_PLAYERS_OPTIONS } from "@/data/staticData";
+import { SORT_PLAYERS_OPTIONS } from "@/data/staticData";
 import { createQueryString, removeQueryString } from "@/lib/queryParams";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useSelector } from "react-redux";
@@ -13,11 +12,11 @@ import s from "./FiltersSection.module.scss";
 const FiltersSection = () => {
   const { allPlayersData, playersScroll } = useSelector((s) => s.players);
 
-  const router = useRouter();
-  const pathname = usePathname();
   const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+
   const sortBy = searchParams?.get("sort") || "last-seen";
-  const filterBy = searchParams?.get("badge") || "all";
 
   function handleSortChange(newValue) {
     const isDefault = newValue === "last-seen";
@@ -29,11 +28,6 @@ const FiltersSection = () => {
     if (!isDefault) {
       createQueryString("sort", newValue, searchParams, router, pathname);
     }
-  }
-
-  function handleFilterChange(event) {
-    const newValue = event.target.value;
-    createQueryString("badge", newValue, searchParams, router, pathname);
   }
 
   return (
@@ -81,18 +75,6 @@ const FiltersSection = () => {
                 inputMode="numeric"
               />
             </div>
-          </div>
-
-          <div className={`${s.filterGroup} ${s.selectGroup}`}>
-            <label className={s.filterLabel} htmlFor="players-filter-badges">
-              Filter By Badges
-            </label>
-            <SelectMenu
-              optionsData={FILTER_PLAYERS_BADGES}
-              onChange={handleFilterChange}
-              value={filterBy}
-              id="players-filter-badges"
-            />
           </div>
 
           <ResultsSummary
