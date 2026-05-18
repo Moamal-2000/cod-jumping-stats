3ca60bc15b45fc2730776e5d36eea6025b938547
Moamal-2000 <moamalalaapro1@gmail.com>
2026-05-19 00:18:31 +0300

    chore(JoinServerButton): simplify code logic

--- src/components/Pages/ServersPage/AllServers/GameType/ServerCard/ServerCardHeader/JoinServerButton/JoinServerButton.jsx
+++ src/components/Pages/ServersPage/AllServers/GameType/ServerCard/ServerCardHeader/JoinServerButton/JoinServerButton.jsx
@@ -4,19 +4,19 @@ import s from "./JoinServerButton.module.scss";
 const isMobileDevice = isMobile();
 
 const JoinServerButton = ({ server }) => {
-  if (isMobileDevice) {
+  if (isMobileDevice || !server.GameType.includes("COD")) {
     return;
   }
 
-  const game = server.GameType === "COD4" ? "cod4" : "cod2x";
+  const game = server.GameType.toLowerCase();
   const serverAddress = `${server.IP}:${server.Port}`;
 
   return (
     <a
-      href={`${game}://%2Bconnect%20${serverAddress}`}
+      className={`${s.joinServerButton} ${s[game]}`}
+      href={`${game}x://%2Bconnect%20${serverAddress}`}
       title={`Connect to ${serverAddress}`}
       aria-label={`Join Server at ${serverAddress}`}
-      className={`${s.joinServerButton} ${s[server.GameType.toLowerCase()]}`}
     >
       Join Server
     </a>
