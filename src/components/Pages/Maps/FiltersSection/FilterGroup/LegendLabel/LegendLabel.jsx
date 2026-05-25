a19631ec02323aa4503cc296a0de1c6d630de1e3
Moamal-2000 <moamalalaapro1@gmail.com>
2026-05-25 07:46:34 +0300

    fix(LegendLabel): hide tooltip trigger when tooltipText is not provided

--- src/components/Pages/Maps/FiltersSection/FilterGroup/LegendLabel/LegendLabel.jsx
+++ src/components/Pages/Maps/FiltersSection/FilterGroup/LegendLabel/LegendLabel.jsx
@@ -4,15 +4,17 @@ const LegendLabel = ({ label, tooltipText }) => {
   return (
     <legend className={s.label} data-label={label}>
       {label}
-      <div className={s.tooltip}>
-        <div className={s.icon}>
-          <svg aria-hidden="true">
-            <use href="/icons-sprite.svg#question-mark" />
-          </svg>
-        </div>
 
-        <p className={s.tooltipText}>{tooltipText}</p>
-      </div>
+      {tooltipText !== undefined && (
+        <div className={s.tooltip}>
+          <div className={s.icon}>
+            <svg aria-hidden="true">
+              <use href="/icons-sprite.svg#question-mark" />
+            </svg>
+          </div>
+          <p className={s.tooltipText}>{tooltipText}</p>
+        </div>
+      )}
     </legend>
   );
 };
