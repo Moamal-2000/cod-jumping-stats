808ef547ddb7079ac149e96896eface57ff36405
Moamal-2000 <moamalalaapro1@gmail.com>
2026-06-16 11:41:35 +0300

    chore(baseQueryMsgPack): simplify implementation by removing unused logic

--- src/redux/msgPack/baseQueryMsgPack.js
+++ src/redux/msgPack/baseQueryMsgPack.js
@@ -3,12 +3,7 @@ import { decodeAsyncData, fetchMsgPackResponse } from "@/lib/api/msgpackClient";
 export const baseQueryMsgPack = async ({ url }) => {
   try {
     const response = await fetchMsgPackResponse({ url });
-    const data = (await decodeAsyncData(response)) ?? {};
-
-    return {
-      ...data,
-      __status: response?.status,
-    };
+    return (await decodeAsyncData(response)) ?? {};
   } catch (error) {
     return { error };
   }
