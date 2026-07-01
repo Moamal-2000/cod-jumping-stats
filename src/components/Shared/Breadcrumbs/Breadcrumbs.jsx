7aa7ad8bbea6c9cd8fe8c4e6b1e74a794a0c1a00
Moamal-2000 <moamalalaapro1@gmail.com>
2026-07-01 20:21:58 +0300

    feat(Breadcrumbs): support server-aware navigation

--- src/components/Shared/Breadcrumbs/Breadcrumbs.jsx
+++ src/components/Shared/Breadcrumbs/Breadcrumbs.jsx
@@ -1,3 +1,4 @@
+import { useSearchParams } from "next/navigation";
 import TransitionLink from "../Links/TransitionLink/TransitionLink";
 import s from "./Breadcrumbs.module.scss";
 
@@ -5,14 +6,22 @@ const Breadcrumbs = ({ labels, paths }) => {
   const previousPages = labels.slice(0, labels.length - 1);
   const currentPage = labels.at(-1);
 
+  const searchParams = useSearchParams();
+  const sourceParam = searchParams.get("source") || "jh";
+
   return (
     <nav className={s.breadcrumbs} data-type="breadcrumbs">
-      {previousPages.map((page, index) => (
-        <div className={s.page} key={index}>
-          <TransitionLink href={paths?.[index]?.path}>{page}</TransitionLink>
-          <span>/</span>
-        </div>
-      ))}
+      {previousPages.map((page, index) => {
+        const path = paths?.[index]?.path;
+        const href = `${path}${sourceParam === "jh" || path === "/" ? "" : `?source=${sourceParam}`}`;
+
+        return (
+          <div className={s.page} key={index}>
+            <TransitionLink href={href}>{page}</TransitionLink>
+            <span>/</span>
+          </div>
+        );
+      })}
 
       <span className={s.currentPage}>{currentPage || "Unknown"}</span>
     </nav>
